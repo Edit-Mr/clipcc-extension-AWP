2d241f4ce0d30b4c0cd3f516254695662f33dac8
Edit Mr <elvismao.070512@gmail.com>
2022-09-27 12:50:50 +0800

    test

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-const { Extension, type, api } = require('clipcc-extension');
-
-class MyExtension extends Extension {
+const {api, type, extension} = require('clipcc-extension');
+class ExampleExtension extends Extension {
     onInit() {
         api.addCategory({
-            categoryId: 'em.awp.category',
+            // 替換為<你的擴充套件id>.category 下同
+            categoryId: 'em.awp.category', 
             messageId: 'em.awp.category',
-            color: '#66CCFF'
+            color: '#339900'
         });
         api.addBlock({
-            opcode: 'em.awp.hello',
+            opcode: 'em.awp.return',
             type: type.BlockType.REPORTER,
-            messageId: 'em.awp.hello',
+            messageId: 'em.awp.return',
             categoryId: 'em.awp.category',
-            function: () => 'Hello, ClipCC!'
+            param: {
+                VALUE: {
+                    type: type.ParameterType.STRING,
+                    default: 'Hello World!'
+                }
+            },
+            function: args => this.ReturnValue(args.VALUE)
+        });
+        api.addBlock({
+            opcode: 'em.awp.helloworld',
+            type: type.BlockType.COMMAND,
+            messageId: 'em.awp.helloworld',
+            categoryId: 'em.awp.category',
+            function: args => this.HelloWorld()
         });
     }
+    onUninit() {
+        api.removeCategory('em.awp.category');
+    }
+    ReturnValue(VALUE) {
+        return VALUE;
+    }
+    HelloWorld() {
+        console.log("Hello World!");
+        alert("Hello World!");
+    }
 }
-
-module.exports = MyExtension;
+module.exports = ExampleExtension;
+//npm run build
